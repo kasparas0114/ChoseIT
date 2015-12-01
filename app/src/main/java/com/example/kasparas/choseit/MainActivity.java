d9668f709643b33c962667e12320d2e7bcf084ff
kasparas0114 <kasparas.stadalnikas@gmail.com>
2015-12-01 15:55:19 +0200

    MaterialDesignInit

--- app/src/main/java/com/example/kasparas/choseit/MainActivity.java
+++ app/src/main/java/com/example/kasparas/choseit/MainActivity.java
@@ -7,26 +7,23 @@ import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.os.Bundle;
 import android.os.Handler;
-
 import com.google.android.gms.maps.model.LatLng;
-
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import android.util.Log;
-
 import java.io.IOException;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements SelectOptions.OnFragmentInteractionListener,
@@ -135,20 +132,62 @@ public class MainActivity extends AppCompatActivity implements SelectOptions.OnF
 
         Log.w(TAG, "onFragmentInteraction");
     }
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+       setContentView(R.layout.splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                setContentView(R.layout.activity_main);
-                changeFragment(R.id.main, new SelectOptions());
-            }
-        }, SPLASH_TIME_OUT);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+       /* getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+       drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);*/
+
+        changeFragment(R.id.main, new SelectOptions());
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                setContentView(R.layout.activity_main);
+//                Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+//                setSupportActionBar(toolbar);
+//                changeFragment(R.id.main, new SelectOptions());
+//            }
+//        }, SPLASH_TIME_OUT);
         Log.w(TAG, "onCreate");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            Toast.makeText(this, "Add something", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (id == R.id.action_map) {
+             Toast.makeText(this, "Map opened", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void button_select_options_next_click(View view) {
